af8d3df0104486a00ffbd6f786777159960ff6d4
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2025-10-20 18:55:36 +0200

    feat: add rest of tests

--- yaml/v1beta2/types_tests.go
+++ yaml/v1beta2/types_tests.go
@@ -17,8 +17,16 @@ var testBuilder = schemautils.DiscriminatedUnionBuilder[TestInline]{
 	Discriminator: "type",
 	Registry: map[string]TestInline{
 		"not_null":        NotNullTest{},
+		"empty":           EmptyTest{},
 		"unique":          UniqueTest{},
 		"accepted_values": AcceptedValuesTest{},
+		"rejected_values": RejectedValuesTest{},
+		"min_max":         MinMaxTest{},
+		"min_value":       MinValueTest{},
+		"max_value":       MaxValueTest{},
+		"freshness":       FreshnessTest{},
+		"relative_time":   RelativeTimeTest{},
+		"business_rule":   BusinessRuleTest{},
 	},
 	RequireDiscriminator: true,
 }
@@ -32,20 +40,79 @@ func (Test) JSONSchema() *jsonschema.Schema {
 }
 
 type (
-	NotNullTest struct {
+	TestBase struct {
 		isTestImpl
 
-		Columns []string `yaml:"columns"`
+		ID       string   `yaml:"id"`
+		Schedule Schedule `yaml:"schedule,omitempty"`
+	}
+	TestWithColumns struct {
+		Columns []string `yaml:"columns" jsonschema:"minLength=1"`
+	}
+	TestWithTime struct {
+		TimePartitionColumn string `yaml:"time_partition_column,omitempty"`
+		TimeWindowSeconds   int32  `yaml:"time_partition_seconds,omitempty"`
+	}
+)
+
+type (
+	NotNullTest struct {
+		TestBase        `yaml:",inline"`
+		TestWithColumns `yaml:",inline"`
+	}
+	EmptyTest struct {
+		TestBase        `yaml:",inline"`
+		TestWithColumns `yaml:",inline"`
 	}
 	UniqueTest struct {
-		isTestImpl
-
-		Columns []string `yaml:"columns"`
+		TestBase        `yaml:",inline"`
+		TestWithColumns `yaml:",inline"`
+		TestWithTime    `yaml:",inline"`
 	}
 	AcceptedValuesTest struct {
-		isTestImpl
+		TestBase `yaml:",inline"`
 
 		Column string   `yaml:"column"`
-		Values []string `yaml:"values"`
+		Values []string `yaml:"values" jsonschema:"minLength=1"`
+	}
+	RejectedValuesTest struct {
+		TestBase `yaml:",inline"`
+
+		Column string   `yaml:"column"`
+		Values []string `yaml:"values" jsonschema:"minLength=1"`
+	}
+	MinMaxTest struct {
+		TestBase `yaml:",inline"`
+
+		Column   string  `yaml:"column"`
+		MinValue float64 `yaml:"min_value"`
+		MaxValue float64 `yaml:"max_value"`
+	}
+	MinValueTest struct {
+		TestBase `yaml:",inline"`
+
+		Column   string  `yaml:"column"`
+		MinValue float64 `yaml:"min_value"`
+	}
+	MaxValueTest struct {
+		TestBase `yaml:",inline"`
+
+		Column   string  `yaml:"column"`
+		MaxValue float64 `yaml:"max_value"`
+	}
+	FreshnessTest struct {
+		TestBase     `yaml:",inline"`
+		TestWithTime `yaml:",inline"`
+	}
+	RelativeTimeTest struct {
+		TestBase `yaml:",inline"`
+
+		Column         string `yaml:"column"`
+		RelativeColumn string `yaml:"relative_column"`
+	}
+	BusinessRuleTest struct {
+		TestBase `yaml:",inline"`
+
+		SQLExpression string `yaml:"sql_expression"`
 	}
 )
